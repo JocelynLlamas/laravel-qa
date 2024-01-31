72207ec22cb87ca1b40d107eb5183ad63eaf7414
Jocelyn Llamas <mjlldlt@hotmail.com>
2024-01-30 19:35:43 -0600

    first commit

--- public/js/app.js
+++ public/js/app.js
@@ -18154,7 +18154,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "\n.spinner[data-v-7ae326fe] {\n    color: #ddd; \n    text-align: center;\n    /* min-width: 109.61px; */\n}\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "\n.spinner[data-v-7ae326fe] {\r\n    color: #ddd; \r\n    text-align: center;\r\n    /* min-width: 109.61px; */\n}\r\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
